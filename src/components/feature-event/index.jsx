0008e9947e674c56fae6d9bf225d03799300fdd6
rahmanshifur <rahmanshifur235@gmail.com>
2024-01-10 23:16:26 +0600

    fixed feature events design

--- src/components/feature-event/index.jsx
+++ src/components/feature-event/index.jsx
@@ -1,23 +1,57 @@
-import NetworkWifi2BarIcon from '@mui/icons-material/NetworkWifi2Bar';
-import { Box, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import EventImg1 from '../../assets/feature-event-img/img1.jpg';
+import EventImg2 from '../../assets/feature-event-img/img2.jpg';
 import CardItem from "../reuseable/card";
 // import EventImg1 from '../../assets/feature-event-img/img1.jpg'
-import EventImg2 from '../../assets/feature-event-img/img2.jpg';
+import CustomButton from "../ui/custom-button";
+import CustomHeading from '../ui/custom-heading';
 
 const FeatureEvent = () => (
-   <Box>
-      <Paper sx={{ display: 'inline-block' }}>
-         <CardItem
-            headingText={'3D Point Cloud Object Recognition'}
-            size={'md'}
-            img={EventImg1}
-            btnClr='white'
-            btnSize='md'
-            btnBgClr={'primary'}
-            btnHref={'/d'}
-         />
-      </Paper>
+   <Box sx={{}}>
+      <Box textAlign={'center'} padding={'20px 0px'}><CustomHeading
+         headingText={'Featured Events'}
+         size={'x-lg'}
+         underline={'lg-underline'}
+      /></Box>
+      <Box sx={{ display: 'flex' }}>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               img={EventImg1}
+               btnClr='white'
+               btnSize='md'
+               btnBgClr={'primary'}
+               btnHref={'/d'}
+            />
+         </Box>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               img={EventImg1}
+               btnClr='white'
+               btnSize='md'
+               btnBgClr={'primary'}
+               btnHref={'/d'}
+            />
+         </Box>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               img={EventImg1}
+               btnClr='white'
+               btnSize='md'
+               btnBgClr={'primary'}
+               btnHref={'/d'}
+            />
+         </Box>
+      </Box>
+      <Box sx={{ textAlign: 'center', padding: '50px 0px' }}><CustomButton
+         btnText={'See All Events'}
+         color='white'
+      /></Box>
       <CardItem
          headingText={'3D Point Cloud Object Recognition'}
          size={'md'}
@@ -26,12 +60,12 @@ const FeatureEvent = () => (
          btnHref={'/a'}
          href={'/aa'}
       />
-      <CardItem
+      {/* <CardItem
          headingText={'NETWORKING'}
          size={'md'}
          icon={<NetworkWifi2BarIcon sx={{ fontSize: 40 }} />}
-      />
-   </Box>
+      /> */}
+   </Box >
 )
 
 export default FeatureEvent
--- src/components/ui/custom-button.jsx
+++ src/components/ui/custom-button.jsx
@@ -13,13 +13,14 @@ export default CustomButton;
 
 const BtnLink = styled(Link)(({ props: { color } }) => ({
    textDecoration: 'none',
-   color: (color === 'white' && "#fff") || (color === 'black' && "#000") || "#000000",
+   color: (color === 'white' && "#FFFFFF") || (color === 'black' && "#000") || "#000000",
    backgroundColor: (color === 'black' && '#FFC936') || (color === 'white' && '#222222') || '#fff',
    fontSize: '16px',
-   fontWeight: 500,
+   fontWeight: 'bold',
    display: 'inline-block',
    boxShadow: '0px 0px 10px -3px rgba(0,0,0,0.5)',
    borderRadius: '5px',
-   padding: '15px 25px'
+   padding: '12px 25px',
+   lineHeight: 1
 }))
 
