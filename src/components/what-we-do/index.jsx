00dd269ea0ed316586e40a84e74eadb78ef7ed92
rahmanshifur <rahmanshifur235@gmail.com>
2024-01-13 11:23:57 +0600

    created what-we-do component succesfuly

---
+++ src/components/what-we-do/index.jsx
@@ -0,0 +1,60 @@
+
+import CastForEducationIcon from '@mui/icons-material/CastForEducation';
+import { Box, styled } from "@mui/material";
+import CardItem from "../reuseable/card";
+import CustomButton from "../ui/custom-button";
+import CustomHeading from '../ui/custom-heading';
+
+const WhatWeDo = () => (
+   <Box sx={{}}>
+      <Box textAlign={'center'} padding={'20px 0px'} margin={'12px 0px'}><CustomHeading
+         headingText={'What We Do'}
+         size={'x-lg'}
+      /></Box>
+      <Box sx={{ display: 'flex' }}>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               icon={<Icon />}
+            />
+         </Box>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               icon={<Icon />}
+            />
+         </Box>
+         <Box sx={{ borderRadius: '15px', padding: '0px 20px' }}>
+            <CardItem
+               headingText={'3D Point Cloud Object Recognition'}
+               size={'md'}
+               icon={<Icon />}
+            />
+         </Box>
+      </Box>
+      <BtnAction>
+         <CustomButton
+            btnText={'Know more about IEEE'}
+         />
+         <CustomButton
+            btnText={'Know more about IEEE NSU SB'}
+         />
+      </BtnAction>
+   </Box >
+)
+
+export default WhatWeDo
+
+
+const Icon = styled((CastForEducationIcon))(() => ({
+   fontSize: '55px',
+   color: '#54595F'
+}))
+const BtnAction = styled((Box))(() => ({
+   textAlign: 'center',
+   padding: '50px 0px 0px 0px',
+   display: 'flex',
+   justifyContent: 'space-around'
+}))
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,14 @@
 import { Box, Container } from "@mui/material"
 import FeatureBlogs from "../../components/feature-blogs"
 import FeatureEvent from "../../components/feature-event"
-import CustomButton from "../../components/ui/custom-button"
-import CustomHeading from "../../components/ui/custom-heading"
-import CustomSummary from "../../components/ui/custom-summary"
-import { shortText } from "../../helper"
+import WhatWeDo from "../../components/what-we-do"
 
 
 const HomePage = () => {
    return (
       <Box>
-         <Box textAlign={'center'}>
-            <CustomHeading href={'/aaa'} size="lg" headingText={'hello heading'} />
-         </Box>
-         <CustomHeading size="lg" underline='md-underline' headingText={'hello heading'} />
-         <CustomHeading size="lg" underline='sm-underline' headingText={'hello heading'} />
-         <CustomHeading size="x-sm" underline='sm-underline' headingText={'hello heading'} />
-
-
-         <CustomButton btnText={'Know more about IEEE'} />
-         <CustomButton btnText={'See All News'} color={'black'} />
-
-         <CustomSummary summary={shortText('Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, commodi impedit! Eum quae aut sapiente ex corporis inventore amet molestias!', 70)} />
          <Container>
+            <WhatWeDo />
             <FeatureEvent />
             <FeatureBlogs />
          </Container>
