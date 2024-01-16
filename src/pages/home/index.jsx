789cf9114c938bdce74628ffc8e740c7cdc2ff80
rahmanshifur <rahmanshifur235@gmail.com>
2024-01-16 22:46:27 +0600

    fixed home page conflige

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,12 +4,8 @@ import CarouselSlider from "../../components/carousel"
 import ContactCopmonent from "../../components/contact-us"
 import FeatureBlogs from "../../components/feature-blogs"
 import FeatureEvent from "../../components/feature-event"
-import WhatWeDo from "../../components/what-we-do"
-<<<<<<< HEAD
-=======
-import ContactCopmonent from "../../components/contact-us"
 import LatestEvent from "../../components/latest-event"
->>>>>>> 80073347a4cf6fa5664548da6b840ffb9c2d9ab4
+import WhatWeDo from "../../components/what-we-do"
 
 
 const HomePage = () => {
@@ -17,7 +13,7 @@ const HomePage = () => {
       <Box>
          <CarouselSlider />
          <WhatWeDo />
-         <LatestEvent/>
+         <LatestEvent />
          <FeatureEvent />
          <FeatureBlogs />
          <FAQSection />
